3bdd1a65556fdaea9b1788419d6b8d06318ab320
BryanFRD <bryanferrando59@gmail.com>
2022-08-25 15:02:13 +0200

    Changes

--- src/components/layouts/nav/SideBarOption.jsx
+++ src/components/layouts/nav/SideBarOption.jsx
@@ -8,7 +8,7 @@ const SideBarOption = ({icon, name, to, isShown, onClick, className, asNavLink})
     className={`sidebaroption d-flex flex-column justify-content-center align-items-center py-3 ${className}`}
     onClick={onClick}>
       {
-        <div className={`d-flex justify-content-end align-items-center gap-2 w-100`}>
+        <div className={`d-flex justify-content-end align-items-center gap-2 w-100 user-select-none`}>
           <span className={!isShown ? 'd-none' : ''}>{name}</span>
           <Icon icon={icon} fontSize='2.5em' className='pe-none'/>
         </div>
