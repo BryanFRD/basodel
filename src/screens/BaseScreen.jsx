87582bc03bd22dac6307fc7c25d4b2f8ed9fd016
Bryan <bryanferrando59@gmail.com>
2022-08-27 20:54:02 +0200

    blink problem when loading new page solved

--- src/screens/BaseScreen.jsx
+++ src/screens/BaseScreen.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Outlet } from 'react-router-dom';
 import Footer from '../components/layouts/Footer';
 import MainNavbar from '../components/layouts/navbar/MainNavbar';
 import Row from 'react-bootstrap/esm/Row';
 import Col from 'react-bootstrap/esm/Col';
+import { ThemeContext } from '../context/ThemeContext';
 
 const BaseScreen = () => {
+  const { theme } = useContext(ThemeContext);
   return (
     <>
-      <Row className='g-0'>
+      <Row className={`g-0 ${theme.bgClass}`}>
         <Col className='order-lg-1' lg='auto'>
           <header>
             <MainNavbar />
