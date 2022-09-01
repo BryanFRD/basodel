5a2d8289b8dc66a26eeff19ee6df8c3acf280088
BryanFRD <bryanferrando59@gmail.com>
2022-09-01 09:37:30 +0200

    Added username in chat

--- src/components/layouts/navbar/secondNavbar/options/chat/ChatMessage.jsx
+++ src/components/layouts/navbar/secondNavbar/options/chat/ChatMessage.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from '../../../../../../context/ThemeContext';
 
-const ChatMessage = ({message}) => {
+const ChatMessage = ({message}) => {  
+  const { theme } = useContext(ThemeContext);
+  
   return (
-    <div className={`chat-message-container ${message.sender ? 'message-sender' : 'message-receiver'}`}>
-      <div className={`chat-message`}>
-        {message.content}
+    <div className={`chat-message-container ${theme.chat} ${message.sender ? 'message-sender' : 'message-receiver'}`}>
+      <div className={`chat-message d-flex flex-column`}>
+        <span className={`chat-username`}>Bernard</span>
+        <span>{message.content}</span>
       </div>
     </div>
   );
