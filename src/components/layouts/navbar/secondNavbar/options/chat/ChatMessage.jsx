63a451cabce38a75dd19df74360ad561853ae79f
Bryan <bryanferrando59@gmail.com>
2022-09-01 21:50:35 +0200

    Add dropdown when cliking on a user

--- src/components/layouts/navbar/secondNavbar/options/chat/ChatMessage.jsx
+++ src/components/layouts/navbar/secondNavbar/options/chat/ChatMessage.jsx
@@ -1,25 +1,33 @@
-import React, { useContext, useRef, useState } from 'react';
+import React, { useContext } from 'react';
 import { Dropdown } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 import { ThemeContext } from '../../../../../../context/ThemeContext';
-import GenerikLink from '../../../../../generic/link/GenericLink';
 
 const ChatMessage = ({message}) => {  
   const { theme } = useContext(ThemeContext);
+  const { t } = useTranslation();
   
   const handleBlockUser = () => {
-    console.log('Blocking: ' + message);
+    console.log(`Blocked: ${JSON.stringify(message)}`);
+  }
+  
+  const handleReportUser = () => {
+    console.log(`Reported: ${JSON.stringify(message)}`);
   }
   
   return (
     <div className={`chat-message-container ${theme.chat} ${message.sender ? 'message-sender' : 'message-receiver'}`}>
       <div className={`chat-message d-flex flex-column`}>
         <Dropdown>
-          <Dropdown.Toggle variant={theme.variant} className={`chat-username mb-2`}>Bernard</Dropdown.Toggle>
+          <Dropdown.Toggle variant={theme.variant} className={`chat-username m-0 p-0 mb-2`}>Bernard</Dropdown.Toggle>
           <Dropdown.Menu variant={theme.variant}>
-            <Dropdown.Item className={theme.textDanger}>
-              Block user
+            <Dropdown.Item className={theme.textDanger} onClick={handleBlockUser}>
+              {t('chat.blockUser')}
             </Dropdown.Item>
-            
+            <Dropdown.Divider />
+            <Dropdown.Item className={theme.textDanger} onClick={handleReportUser}>
+              {t('chat.reportUser')}
+            </Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
         {/* <GenerikLink className={`chat-username mb-2`}>Bernard</GenerikLink> */}
