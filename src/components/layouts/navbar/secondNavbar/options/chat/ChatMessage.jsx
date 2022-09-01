0b84cfb2ca25d5e57f36f5e0e2de3d9ee3e12941
BryanFRD <bryanferrando59@gmail.com>
2022-09-01 14:46:06 +0200

    Balblala PTIT ZGEG

--- src/components/layouts/navbar/secondNavbar/options/chat/ChatMessage.jsx
+++ src/components/layouts/navbar/secondNavbar/options/chat/ChatMessage.jsx
@@ -1,15 +1,32 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
+import { Overlay, Popover } from 'react-bootstrap';
 import { ThemeContext } from '../../../../../../context/ThemeContext';
 import GenerikLink from '../../../../../generic/link/GenericLink';
 
 const ChatMessage = ({message}) => {  
   const { theme } = useContext(ThemeContext);
+  const [ showOverlay, setShowOverlay ] = useState(false);
+  
+  const handleShowPopover = () => {
+    setShowOverlay(prevValue => !prevValue);
+  }
+  
+  const handleBlockUser = () => {
+    console.log('Blocking: ' + message);
+  }
   
   return (
     <div className={`chat-message-container ${theme.chat} ${message.sender ? 'message-sender' : 'message-receiver'}`}>
       <div className={`chat-message d-flex flex-column`}>
-        <GenerikLink className={`chat-username mb-2`}>Bernard</GenerikLink>
-        <span>{message.content}</span>
+        <GenerikLink className={`chat-username mb-2`} onClick={handleShowPopover}>Bernard</GenerikLink>
+        <Overlay
+          show={showOverlay}
+          placement='bottom'>
+          <Popover>
+            <Popover.Header as='h3'>Bernard-Henri</Popover.Header>
+          </Popover>
+        </Overlay>
+        <span>{message.content + "Coucou c'est moi !"}</span>
       </div>
     </div>
   );
