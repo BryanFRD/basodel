e7b106bfdb4ad6db64f82a1ff8aa885de22840c9
Bryan <bryanferrando59@gmail.com>
2022-10-13 00:11:41 +0200

    Changes

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   const { theme } = useContext(ThemeContext);
   
   return (
-    <Container fluid className={`p-3 text-center fw-light ${theme.bgClass} ${theme.text}`}>
+    <Container fluid className={`p-3 text-center fw-light themed-background themed-color`}>
       <div>©&nbsp;2022&nbsp;Copyright: &nbsp;
         <a href='https://bryan-ferrando.fr'
         target='_blank'
