f708f946a84f961460c72e1ff3109aa3e0a996cc
Bryan <bryanferrando59@gmail.com>
2022-11-27 10:48:13 +0100

    Changes

--- src/context/SocketContext.jsx
+++ src/context/SocketContext.jsx
@@ -1,17 +1,15 @@
-import Cookies from 'js-cookie';
 import React, { useEffect, useContext } from 'react';
 import { io } from 'socket.io-client';
 import Config from '../config/Config';
 import { UserContext } from './UserContext';
 
 export const SocketContext = React.createContext();
-const socket = io(Config.API.URL, {auth: {}});
+const socket = io(Config.API.URL, {auth: {}, withCredentials: true});
 
 const SocketContextProvider = (props) => {
   const {user} = useContext(UserContext);
   
   useEffect(() => {
-    socket.auth.token = Cookies.get('accessToken');
     socket.connect();
     
     return () => socket.disconnect();
