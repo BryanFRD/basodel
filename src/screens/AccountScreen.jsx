c5ce050cb6e5bf0492f82b34161ee747caf99076
Bryan <bryanferrando59@gmail.com>
2023-01-03 15:16:15 +0100

    Changes

--- src/screens/AccountScreen.jsx
+++ src/screens/AccountScreen.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useContext } from 'react';
+import { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../context/UserContext';
 
 const AccountScreen = () => {
   const {t} = useTranslation();
+  const {user} = useContext(UserContext);
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    if(!user){
+      navigate('/');
+    }
+  }, [user, navigate]);
   
   return (
     <Container fluid className='h-100 d-flex justify-content-center align-items-center'>
